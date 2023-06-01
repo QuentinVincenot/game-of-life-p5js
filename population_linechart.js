34f77c7f85b72ed3b85a8e1931c4df76ac548ace
Quentin Vincenot <quentin.vincenot@gmail.com>
2023-06-01 21:18:59 +0200

    Implemented the next game step main function

--- population_linechart.js
+++ population_linechart.js
@@ -4,7 +4,7 @@
 
 function initialize_population_linechart(labels, population) {
     // Create and initialize the datapoints to plot the line chart
-    var datapoints = {
+    let datapoints = {
         labels: labels,
         datasets: [{
             label: 'Population',
@@ -17,10 +17,10 @@ function initialize_population_linechart(labels, population) {
     };
 
     // Retrieve the canvas from the HTML DOM structure, to create the chart inside
-    var context = document.getElementById('line_chart_canvas').getContext('2d');
+    let context = document.getElementById('line_chart_canvas').getContext('2d');
 
     // Create the line chart with the right configuration
-    var line_chart = new Chart(context, {
+    let line_chart = new Chart(context, {
         type: 'line',
         data: datapoints,
         options: {
